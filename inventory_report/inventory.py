f29ff18128aa99cead60cb2f93e55bae09b02ee9
andrew-marquezin <andrewmmarinho@gmail.com>
2024-06-12 19:28:47 -0300

    requisito 5 feito

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: list[Product] | None = None) -> None:
+        self._data = [] if data is None else data
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data
+
+    def add_data(self, data: list[Product]) -> None:
+        for product in data:
+            self._data.append(product)
