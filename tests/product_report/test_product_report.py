1af46b485ea34e794c3a628e9e105d183c6213ae
andrew-marquezin <andrewmmarinho@gmail.com>
2024-06-12 18:42:33 -0300

    requisito 2 feito

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,28 @@
-def test_product_report() -> None:
-    raise NotImplementedError
+import pytest
+
+from inventory_report.product import Product
+
+
+def test_product_report(capsys: pytest.CaptureFixture) -> None:
+    farinha = Product(
+        '2',
+        'farinha',
+        'Farinini',
+        '01-05-2021',
+        '02-06-2023',
+        'TY68 409C JJ43 ASD1 PL2F',
+        'precisa ser armazenado em local protegido da luz'
+    )
+    print(str(farinha))
+
+    captured = capsys.readouterr()
+
+    assert captured.out == (
+            f"The product {farinha.id} - {farinha.product_name} "
+            f"with serial number {farinha.serial_number} "
+            f"manufactured on {farinha.manufacturing_date} "
+            f"by the company {farinha.company_name} "
+            f"valid until {farinha.expiration_date} "
+            "must be stored according to the following instructions: "
+            f"{farinha.storage_instructions}.\n"
+    )
