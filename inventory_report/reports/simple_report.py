8f7daaf37de11c585c7cb92a6ab4b76561435609
andrew-marquezin <andrewmmarinho@gmail.com>
2024-06-13 14:45:22 -0300

    requisito 7 feito

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,37 @@
+from datetime import date
+from collections import Counter
+
+from inventory_report.inventory import Inventory
+
+
 class SimpleReport:
-    pass
+    def __init__(self) -> None:
+        self.data = list()
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.data.append(inventory)
+
+    def generate(self) -> str:
+        products = [
+            product for inventory in self.data for product in inventory.data
+        ]
+
+        closest_expiration = min(
+            product.expiration_date
+            for product in products
+            if product.expiration_date > date.today().strftime("%Y-%m-%d")
+        )
+
+        oldest_manufacturing = min(
+            product.manufacturing_date for product in products
+        )
+
+        contagem = Counter(product.company_name for product in products)
+
+        most_common_manufacturer = contagem.most_common(1)[0][0]
+
+        return (
+            f"Oldest manufacturing date: {oldest_manufacturing}"
+            f"Closest expiration date: {closest_expiration}"
+            f"Company with the largest inventory: {most_common_manufacturer}"
+        )
